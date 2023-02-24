370caaeb10a87f1d74810e509db2e7a3e660baa5
Sameer <sameer4363@gmail.com>
2023-02-23 19:06:31 -0500

    Add webpack files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log('Weather App page loaded');
+// getWeather(null);
+
+// async function getWeather(location) {
+//   const weatherData = await fetch(
+//     'https://api.openweathermap.org/data/2.5/weather?q=London&APPID=d8990a92b3e6fcbc8aebc61fc2f193ec'
+//   );
+//   console.log(weatherData);
+// }
